9284386cce8141569423ebd1019408070c157244
jcarlosdamian <jcarlosdamian@9c03a204-3402-e9ec-4202-9ef84f67120a>
2010-09-22 05:35:26 +0000

    Primer Import

---
+++ trunk/VentaPasajesBus/src/main/java/pe/edu/upc/dew/ventapasajesbus/model/Pasaje.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pe.edu.upc.dew.ventapasajesbus.model;
+
+/**
+ *
+ * @author Juan
+ */
+public class Pasaje {
+
+}
