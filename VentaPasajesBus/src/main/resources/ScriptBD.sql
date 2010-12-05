52c2019f8bcdce26eb7317999b27a07def9a106f
ybricen.b <ybricen.b@gmail.com>
2010-12-05 03:43:34 +0000

    Actualización del Script BD

--- VentaPasajesBus/src/main/resources/ScriptBD.sql
+++ VentaPasajesBus/src/main/resources/ScriptBD.sql
@@ -1,12 +1,10 @@
-drop database bus;
-
+DROP DATABASE bus;
 CREATE DATABASE IF NOT EXISTS bus;
 USE bus;
 
 CREATE TABLE `bus`.`empresatransporte` (
   `Co_EmpresaTransporte` INTEGER UNSIGNED NOT NULL AUTO_INCREMENT,
   `No_EmpresaTransporte` CHAR(60) NOT NULL,
-  `Co_Placa` CHAR(10) NOT NULL,
   PRIMARY KEY (`Co_EmpresaTransporte`)
 )
 ENGINE = InnoDB;
@@ -57,6 +55,8 @@ CREATE TABLE `bus`.`ruta` (
   `Ss_Tarifa` DOUBLE NOT NULL,
   `Co_EmpresaTransporte` INTEGER UNSIGNED NOT NULL,
   `Co_Placa` CHAR(10) NOT NULL,
+  `Qt_CapacidadTotal` INTEGER UNSIGNED NOT NULL,
+  `Qt_CapacidadDisp` INTEGER UNSIGNED NOT NULL,
   PRIMARY KEY (`Co_Ruta`)
 )
 ENGINE = InnoDB;
@@ -65,6 +65,7 @@ ENGINE = InnoDB;
 CREATE TABLE `bus`.`bus` (
   `Co_Placa` CHAR(10) NOT NULL,
   `Qt_Capacidad` INTEGER UNSIGNED NOT NULL,
+  `Co_EmpresaTransporte` INTEGER UNSIGNED NOT NULL,
   PRIMARY KEY (`Co_Placa`)
 )
 ENGINE = InnoDB;
@@ -72,12 +73,11 @@ ENGINE = InnoDB;
 
 
 CREATE TABLE `bus`.`usuario` (
-  `Co_Usuario` CHAR(10) NOT NULL,
   `No_Username` CHAR(8) NOT NULL,
   `No_Password` CHAR(20) NOT NULL,
   `No_Rol` CHAR(60) NOT NULL,
   `Co_EmpresaTransporte` INTEGER UNSIGNED NOT NULL,
-  PRIMARY KEY (`Co_Usuario`)
+  PRIMARY KEY (`No_Username`)
 )
 ENGINE = InnoDB;
 
@@ -136,17 +136,22 @@ ALTER TABLE `bus`.`usuario` ADD CONSTRAINT `FK_usuario_1` FOREIGN KEY `FK_usuari
     ON UPDATE RESTRICT;
 
 
-ALTER TABLE `bus`.`empresatransporte` ADD CONSTRAINT `FK_empresatransporte_1` FOREIGN KEY `FK_empresatransporte_1` (`Co_Placa`)
-    REFERENCES `bus` (`Co_Placa`)
+ALTER TABLE `bus`.`bus` ADD CONSTRAINT `FK_empresatransporte_1` FOREIGN KEY `FK_empresatransporte_1` (`Co_EmpresaTransporte`)
+    REFERENCES `empresatransporte` (`Co_EmpresaTransporte`)
     ON DELETE RESTRICT
     ON UPDATE RESTRICT;
 
+INSERT INTO empresatransporte VALUES('1','Turismo Barranca S.A.');
+INSERT INTO empresatransporte VALUES('2','Sagal S.A.C');
+INSERT INTO empresatransporte VALUES('3','Punto C');
+INSERT INTO empresatransporte VALUES('4','Speedy S.A');
+INSERT INTO empresatransporte VALUES('5','Rapido y Furioso EIRL.');
 
-INSERT INTO bus VALUES ('WMX-0001','44');
-INSERT INTO bus VALUES ('WMX-0002','40');
-INSERT INTO bus VALUES ('WMX-0003','35');
-INSERT INTO bus VALUES ('WMX-0004','41');
-INSERT INTO bus VALUES ('WMX-0005','40');
+INSERT INTO bus VALUES ('WMX-0001','44', '1');
+INSERT INTO bus VALUES ('WMX-0002','40', '2');
+INSERT INTO bus VALUES ('WMX-0003','35', '3');
+INSERT INTO bus VALUES ('WMX-0004','41', '4');
+INSERT INTO bus VALUES ('WMX-0005','40', '5');
 
 
 INSERT INTO ciudad VALUES ('1','Barranca');
@@ -191,17 +196,11 @@ INSERT INTO asiento VALUES ('31');
 INSERT INTO asiento VALUES ('32');
 
 
-INSERT INTO empresatransporte VALUES('1','Turismo Barranca S.A.','WMX-0001');
-INSERT INTO empresatransporte VALUES('2','Sagal S.A.C','WMX-0002');
-INSERT INTO empresatransporte VALUES('3','Turismo Barranca S.A.','WMX-0003');
-INSERT INTO empresatransporte VALUES('4','Speedy S.A','WMX-0004');
-INSERT INTO empresatransporte VALUES('5','Rapido y Furioso EIRL.','WMX-0005');
-
-INSERT INTO usuario VALUES('us0001','ybricen','12345','Administrador','1');
-INSERT INTO usuario VALUES('us0002','wbustos','12345','Vendedor','1');
-INSERT INTO usuario VALUES('us0003','lsabina','12345','Vendedor','2');
-INSERT INTO usuario VALUES('us0004','nsanche','12345','Administrador','3');
-INSERT INTO usuario VALUES('us0005','drodrig','12345','Vendedor','5');
+INSERT INTO usuario VALUES('ybricen','12345','Administrador','1');
+INSERT INTO usuario VALUES('wbustos','12345','Vendedor','1');
+INSERT INTO usuario VALUES('lsabina','12345','Vendedor','2');
+INSERT INTO usuario VALUES('nsanche','12345','Administrador','3');
+INSERT INTO usuario VALUES('drodrig','12345','Vendedor','5');
 
 INSERT INTO cliente VALUES('cl0001','Yessel Briceno','Jr.Aguarico 649','2354186','ybricen.b@gmail.com','1');
 INSERT INTO cliente VALUES('cl0002','Nicolas Sanchez','Av. Ica 578','9865241','utnico@gmail.com','2');
@@ -212,14 +211,44 @@ INSERT INTO cliente VALUES('cl0006','Martin Ramirez','Jr.Aguarico 555','2352143'
 INSERT INTO cliente VALUES('cl0007','Carlos Morales','Jr.Aguarico 875','2351256','cmorales@gmail.com','4');
 
 
-INSERT INTO ruta VALUES('1','7','1','2010-05-24 08:00','2010-05-25 15:00','524.23','2','WMX-0005');
-INSERT INTO ruta VALUES('2','2','3','2010-05-25 09:00','2010-05-25 14:00','225.31','2','WMX-0004');
-INSERT INTO ruta VALUES('3','5','6','2010-05-26 10:00','2010-05-26 13:00','623.45','3','WMX-0003');
-INSERT INTO ruta VALUES('4','2','1','2010-05-27 11:00','2010-05-27 16:00','524.11','1','WMX-0002');
-INSERT INTO ruta VALUES('5','7','6','2010-05-28 12:00','2010-05-28 15:00','524.23','1','WMX-0001');
+INSERT INTO ruta VALUES('1','7','1','2010-05-24 08:00','2010-05-25 15:00','524.23','2','WMX-0005', '40', '40');
+INSERT INTO ruta VALUES('2','2','3','2010-05-25 09:00','2010-05-25 14:00','225.31','2','WMX-0004', '30', '30');
+INSERT INTO ruta VALUES('3','5','6','2010-05-26 10:00','2010-05-26 13:00','623.45','3','WMX-0003', '20', '20');
+INSERT INTO ruta VALUES('4','2','1','2010-05-27 11:00','2010-05-27 16:00','524.11','1','WMX-0002', '40', '40');
+INSERT INTO ruta VALUES('5','7','6','2010-05-28 12:00','2010-05-28 15:00','524.23','1','WMX-0001', '45', '45');
 
 INSERT INTO reserva VALUES('1','0','1','1','cl0001','1');
 INSERT INTO reserva VALUES('2','0','30','2','cl0007','2');
 INSERT INTO reserva VALUES('3','1','25','3','cl0005','3');
 INSERT INTO reserva VALUES('4','1','3','3','cl0001','4');
 INSERT INTO reserva VALUES('5','1','18','5','cl0004','5');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
