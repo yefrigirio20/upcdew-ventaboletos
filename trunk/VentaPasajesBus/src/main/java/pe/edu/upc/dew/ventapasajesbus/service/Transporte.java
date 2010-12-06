b2b2722c8fff76233b9e889d3d7f99212397c0ea
jcarlosdamian <jcarlosdamian@9c03a204-3402-e9ec-4202-9ef84f67120a>
2010-12-06 02:51:08 +0000

    login

--- trunk/VentaPasajesBus/src/main/java/pe/edu/upc/dew/ventapasajesbus/service/Transporte.java
+++ trunk/VentaPasajesBus/src/main/java/pe/edu/upc/dew/ventapasajesbus/service/Transporte.java
@@ -1,6 +1,7 @@
 package pe.edu.upc.dew.ventapasajesbus.service;
 
 import java.util.List;
+import pe.edu.upc.dew.ventapasajesbus.dao.Empresatransporte;
 import pe.edu.upc.dew.ventapasajesbus.dao.Ruta;
 
 public interface Transporte {
@@ -9,6 +10,8 @@ public interface Transporte {
                 String horallegada, String tarifa, String bus);
     public Ruta getRuta();
     public List<Ruta> getRutas();
+    public List<Ruta> getRutasByEmpresaDeTransporte(Empresatransporte emp);
     public String getMensaje();
+
 
 }
