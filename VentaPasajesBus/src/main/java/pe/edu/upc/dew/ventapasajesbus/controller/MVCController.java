4adbf9388a144b7551d8cb2b8bb5eef93ac45d5d
jcarlosdamian <jcarlosdamian@9c03a204-3402-e9ec-4202-9ef84f67120a>
2010-12-06 00:12:03 +0000

    reservas

--- VentaPasajesBus/src/main/java/pe/edu/upc/dew/ventapasajesbus/controller/MVCController.java
+++ VentaPasajesBus/src/main/java/pe/edu/upc/dew/ventapasajesbus/controller/MVCController.java
@@ -1,16 +1,30 @@
 package pe.edu.upc.dew.ventapasajesbus.controller;
 
+import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractController;
+import pe.edu.upc.dew.ventapasajesbus.dao.Ruta;
+import pe.edu.upc.dew.ventapasajesbus.service.Transporte;
+import pe.edu.upc.dew.ventapasajesbus.service.TransporteImpl;
 
 public class MVCController extends AbstractController{
 
     @Override
     protected ModelAndView handleRequestInternal(HttpServletRequest req, HttpServletResponse res) throws Exception {
         HttpSession session = req.getSession();
+
+        List<Ruta> rutas;
+        Transporte emp = new TransporteImpl();
+
+        rutas = emp.getRutas();
+
+        session.setAttribute("rutas", rutas);
+        session.setAttribute("rutas1", "rutas1");
+
+
         return new ModelAndView("reserva");
     }
 
